c3f78814dc72716a876308a6cabda86265b6bd12
nawab067 <haidernawab067@gmail.com>
2025-06-01 23:54:43 -0700

    ChatBot

--- main.py
+++ main.py
@@ -1,48 +1,32 @@
-import os
 import streamlit as st
-from dotenv import load_dotenv
+from streamlit_option_menu import option_menu
 import google.generativeai as gen_ai
 
-
-load_dotenv()
-
-
-st.set_page_config(
-    page_title="Start with Gemini",
-    page_icon="🧠",
-    layout="centered"
-)
-
-
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
+GOOGLE_API_KEY = st.secrets["google"]["api_key"]
 
 if not GOOGLE_API_KEY:
-    st.error("Google API key not found. Please check your .env file.")
+    st.error("Google API key not found.")
 else:
-   
     gen_ai.configure(api_key=GOOGLE_API_KEY)
 
-    # Load Gemini model (for example: gemini-pro)
     model = gen_ai.GenerativeModel("gemini-2.0-flash")
-    
-    def map_role(role):
-        return "assistant" if role == "model"else role
-    
-    if "chat_session" not in st.session_state:
-        st.session_state.chat_session= model.start_chat(history=[])
 
+    def map_role(role):
+        return "assistant" if role == "model" else role
+
+    if "chat_session" not in st.session_state:
+        st.session_state.chat_session = model.start_chat(history=[])
+
+    st.set_page_config(page_title="Start with Gemini", page_icon="🧠", layout="centered")
     st.title("Start with Gemini")
-    
+
     for message in st.session_state.chat_session.history:
-       with st.chat_message(map_role(message.role)):
-           st.markdown(message.parts[0].text)
-    
-    user_input= st.chat_input("Type Your Query Here.......")
+        with st.chat_message(map_role(message.role)):
+            st.markdown(message.parts[0].text)
+
+    user_input = st.chat_input("Type Your Query Here.......")
     if user_input:
         st.chat_message("user").markdown(user_input)
-        response= st.session_state.chat_session.send_message(user_input)
+        response = st.session_state.chat_session.send_message(user_input)
         with st.chat_message("assistant"):
             st.markdown(response.text)
-
-    
